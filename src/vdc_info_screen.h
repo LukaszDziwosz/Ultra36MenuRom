0202a3e201199d8fa5043eb1ef4517c1dea3b38e
Lukasz Dziwosz <c64midi@gmail.com>
2025-06-14 01:15:49 +0200

    removed junk from h file

--- src/vdc_info_screen.h
+++ src/vdc_info_screen.h
@@ -2,7 +2,5 @@
 #define VDC_INFO_SCREEN_H
 
 void draw_vdc_info_screen(unsigned char screen_width);
-extern unsigned char VDC_value;       // Exposed global from assembly
-extern void DetectVDCMemSize(void);   // Exposed function
 
 #endif
